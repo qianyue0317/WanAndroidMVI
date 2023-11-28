10ec82d03d7cd608f8e04a13797cafaf5d29b6e8
qianyue0317 <1974765766@qq.com>
2023-11-28 23:14:53 +0800

    Fragment添加日志

--- app/src/main/java/com/qianyue/wanandroidmvi/base/BaseFragment.kt
+++ app/src/main/java/com/qianyue/wanandroidmvi/base/BaseFragment.kt
@@ -1,11 +1,13 @@
 package com.qianyue.wanandroidmvi.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.qianyue.wanandroidmvi.utils.WanLog
 import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.launch
 
@@ -17,6 +19,29 @@ abstract class BaseFragment<VM : BaseViewModel<*, *>> : Fragment() {
     val vm: VM by lazyVM()
 
     abstract fun lazyVM(): Lazy<VM>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WanLog.d(msg = "${javaClass.simpleName}-onCreate")
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        WanLog.d(msg = "${javaClass.simpleName}-onAttach")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        WanLog.d(msg = "${javaClass.simpleName}-onDetach")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        WanLog.d(msg = "${javaClass.simpleName}-onDestroy")
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
